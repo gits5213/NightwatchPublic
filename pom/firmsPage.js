e273082360354b6dcef57137ade9c0fe1e9e2187
uonwudiwe <uche.onwudiwe@c9tec.com>
2016-02-24 17:05:02 -0500

    working on adding firms

--- pom/firmsPage.js
+++ pom/firmsPage.js
@@ -21,6 +21,22 @@ module.exports = {
 				selector: '//*[@id="ng-view"]/div[2]/div[1]/ul/li[1]/i',
 				locateStrategy: 'xpath'
 			},
+			firmName: 'input[name=name]',
+			street1 : 'input[name=street1]',
+			street2 : 'input[name=street2]',
+			city : 'input[name=city]',
+			state: 'input[name=state]',
+			zip: 'input[name=zip',
+			pfname: 'input[name=provisionerFirstName]',
+			plname: 'input[name=provisionerLastName]',
+			pemail: 'input[name=provisionerEmail]',
+			pwork: 'input[name=work]',
+			pmobile: 'input[name=mobile',
+			
+			
+			
+				
+			
 			okButton:{
 				selector: "(//button[@type='button'])[4]",
 				locateStrategy: 'xpath'
