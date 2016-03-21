5e3809afa25d25a21bc06f6a13dbf3dd9d53543e
uonwudiwe <uche.onwudiwe@c9tec.com>
2016-03-21 09:27:11 -0400

    pointing tests to "dev" branch

--- data/globals.js
+++ data/globals.js
@@ -2,7 +2,7 @@ module.exports = {
 		adminUsername: 'ucheonwudiwe',
 		adminPassword: 'Password1960',
 		email1: 'uche.onwudiwe@c9tec.com',
-		baseUrl: 'https://dev-c9portal.xhoot.com/c9portalMain',
+		baseUrl: 'https://dev-c9portal.xhoot.com/c9portal',
 		
 		ip: '128.177.23.2'
 }
