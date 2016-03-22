f0d35b8817dcf037c885f2bf08e072426ca26e3a
uonwudiwe <uche.onwudiwe@c9tec.com>
2016-03-22 08:08:27 -0400

    reverting back to "main"

--- data/globals.js
+++ data/globals.js
@@ -2,7 +2,7 @@ module.exports = {
 		adminUsername: 'ucheonwudiwe',
 		adminPassword: 'Password1960',
 		email1: 'uche.onwudiwe@c9tec.com',
-		baseUrl: 'https://dev-c9portal.xhoot.com/c9portal',
+		baseUrl: 'https://dev-c9portal.xhoot.com/c9portalMain',
 		
 		ip: '128.177.23.2,10.0.0.40'
 }
