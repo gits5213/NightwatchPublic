55f681343b7de93c086d36ca6fdfe42013f47a36
uonwudiwe <uche.onwudiwe@c9tec.com>
2016-06-23 10:19:05 -0400

    updating ip addresses

--- data/globals.js
+++ data/globals.js
@@ -8,5 +8,11 @@ module.exports = {
 		nonAdminUser: 'johnakpan',
 		nonAdminPass:'Password123',
 		
-		ip: '128.177.23.2,10.0.0.40'
+		ip: '209.66.96.50,10.0.0.40'
+		/*
+		 * Windows machine for CI
+		 * Dev1-selenium.xhoot.com
+		 * Ip = 52.70.102.147
+		 * Internal ip = 10.0.0.40
+		 */
 }
