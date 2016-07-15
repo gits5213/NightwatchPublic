f1f771a43d76028c41a8ed0369d54d0921847d9e
mzaman <mzaman@C9LTW7-22.C9TEC.LOCAL>
2016-07-15 13:03:05 -0400

    adding and modifying the assertions

--- tests/funtionalTest/connectionsTest.js
+++ tests/funtionalTest/connectionsTest.js
@@ -25,7 +25,7 @@ module.exports ={
 			connectionsPage.go();
 			connectionsPage.addIntConnForFirm(dateString,client);
 			
-			client.assert.urlContains('#/addConnection');
+			client.assert.urlContains('#/connections');
 			
 			connectionsPage.editFirstRecord();
 			client.assert.urlContains('#/editConnection');
