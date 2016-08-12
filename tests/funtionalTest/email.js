aec1a80a0cbd535aaee3844607a6911dbbce35d7
Uche Onwudiwe <uonwudiwe@C9TEC.LOCAL>
2016-08-12 16:11:18 -0400

    adding client object

--- tests/funtionalTest/email.js
+++ tests/funtionalTest/email.js
@@ -2,7 +2,7 @@ module.exports ={
 		'Load Microsoft Page': function(client){
 			//var code = client.globals.checkEmail(client);
 			var msftPage = client.page.microsoftonline();
-			msftPage.go();
+			msftPage.go(client);
 			
 			//client.end();
 		}
