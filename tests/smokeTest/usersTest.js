500a874179f22ffc938d0718f3e9dfd825628aac
mzaman <mzaman@C9LTW7-22.C9TEC.LOCAL>
2016-08-25 16:46:41 -0400

    updated

--- tests/smokeTest/usersTest.js
+++ tests/smokeTest/usersTest.js
@@ -16,7 +16,8 @@ module.exports ={
 		usersPage.go(client);
 			client.pause(2000);
 			client.elements('xpath','//*[@id="scrollable-area"]/table/tbody/tr',function(result){
-			client.verify.notEqual(result.value.length, 25, 'There should be more than 25 groups on this page');
+				client.verify.notEqual(result.value.length, 25, 'There should be more than 25 groups on this page');
+			});
 				});
 			client.pause(1000);
 
