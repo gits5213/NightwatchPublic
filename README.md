d52fdf3b03fd7d580def64798f15c50b3e30fecf
Md <mshahnewaz1@gmail.com>
2017-03-19 20:22:11 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # README #
 
-This repository contains the scripts intended for testing the C9 Portal.
+This repository contains the scripts intended for testing the remoteITUniversity website.
 
-More details about configuration, running and developing tests in this repo can be found at: https://6w46h65ghw56gh7.atlassian.net/wiki/display/QA/C9+Portal+Automation
+More details about configuration, running and developing tests in this repo can be found 
+at: http://nightwatchjs.org/ (Visit : www.remoteituniversity.com) 
--- data/globals.js
+++ data/globals.js
@@ -1,33 +1,27 @@
 module.exports = {
 		
 		
-		ip: '209.66.96.50,10.0.0.40',
-		"email1":"qatest.user@c9tec.com",
-        "OnSipDomainName":"c9tec.OnSip.com",
-        "CiscoDomainName1":"173.226.129.163",
-        "CiscoDomainName2":"173.226.129.164",
-        "adminFirm":"Select a",
-        "URI":"http://www.c9tec.com",
-        "CorrectUserName":"testadmin2",
-        "WrongPassword":"abcde1254@",
-        "CorrectPassword":"Password123",
+		ip: '0000000000000000000',
+		"email1":"RemoteITUniversity@gmail.com",
+        "URI":"http://www.remoteituniversity.com",
+
 		
 		msft_credentials : {
-			  clientID: '44303342-65df-4a5d-a4b9-5c08a17002d2',
-			  clientSecret: 'TXDKeT1049qLTjfafVZnAqE',
+			  clientID: 'sdfeed-fghyu-dfrtyu',
+			  clientSecret: 'sdertyujqawASWasdeWERTYU',
 			  site: 'https://login.microsoftonline.com/common',
 			  authorizationPath: '/oauth2/v2.0/authorize',
 			  tokenPath: '/oauth2/v2.0/token'
 			},
 			
-		redirect_uri: 'https://qa1-c9portal.xhoot.com/c9portal',
+		redirect_uri: 'https://qa1-Remotitschool.xhoot.com/remoteituniversity',
 			
 		oauth2 : [],
 		code : '',
 		token : '',
 		scopes: ['openid','https://outlook.office.com/mail.read'],
-		msft_email: 'qatest.user@c9tec.com',
-		msft_email_pass: 'Welcome2C9',
+		msft_email: 'RemoteITUniversity@gmail.com',
+		msft_email_pass: 'Welcome2RemoteIT',
 		queryParams : {
 		        '$select': 'Subject,ReceivedDateTime,From,Body',
 		        '$orderby': 'ReceivedDateTime desc',
